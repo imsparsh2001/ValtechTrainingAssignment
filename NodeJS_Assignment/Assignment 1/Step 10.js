ace9f9015bc63dd2d6932cdd61b87c52e2aee14c
imsparsh2001 <145936464+imsparsh2001@users.noreply.github.com>
2023-10-13 01:14:23 +0530

    Update Step 10.js

--- NodeJS_Assignment/Assignment 1/Step 10.js
+++ NodeJS_Assignment/Assignment 1/Step 10.js
@@ -2,6 +2,8 @@ const fs = require("node:fs");
 const herolist = require("./assets/herolist.json");
 const data = require("./data/data.json");
 const cities = require("./data/city.json");
+const heroImages = require("./data/hero_images.json");
+
 
 
 //Generating Random hero title
@@ -13,11 +15,14 @@ const heroPower = Math.round(Math.random() * 100) ;
 //Generating Random hero city
 const heroCity = cities[Math.round(Math.random() * cities.length )].city;
 
+const heroPoster = heroImages[heroTitle];
+
+
 const newHero = {
     "title": heroTitle,
     "power": heroPower,
     "city": heroCity,
-    "poster": "" 
+    "poster": heroPoster
 };
 
 data.avengers.push(newHero);
