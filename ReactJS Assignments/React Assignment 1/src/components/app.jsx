9cf9967cbd50ad22e2df872cb08a7a333b43e9ed
Sparsh <145936464+imsparsh2001@users.noreply.github.com>
2023-11-13 11:23:04 +0530

    Add files via upload

---
+++ ReactJS Assignments/React Assignment 1/src/components/app.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "./header";
+import Main from "./main";
+import Footer from "./footer";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Main />
+        <Footer />
+      </div>
+    );
+  }
+}
+
+export default App;
